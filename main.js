a9f2f2b2b0fa45d5fd95c5d8a938711df26639f6
applemango <apple.mangoooo97@gmail.com>
2022-04-19 07:02:35 +0900

    2022/04/19 youtube test

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+function getUrlQueries() {
+    var queryStr = window.location.search.slice(1);
+        queries = {};
+    if (!queryStr) {return queries;}
+    queryStr.split('&').forEach(function(queryStr) {
+        var queryArr = queryStr.split('=');
+        queries[queryArr[0]] = queryArr[1];
+    });
+    return queries;
+}
+
 window.onload = function() {
-    document.querySelector("body").innerHTML = "<iframe width='560' height='315' src='https://www.youtube-nocookie.com/embed/lJIrF4YjHfQ' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+    /*document.querySelector("body").innerHTML += "<iframe width='560' height='315' src='https://www.youtube-nocookie.com/embed/lJIrF4YjHfQ' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"*/
+    document.querySelector("body").innerHTML += "<iframe width='560' height='315' src='https://www.youtube-nocookie.com/embed/"+getUrlQueries()["q"]+"' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
 };
